f1dfe279c35083ac2656ba1e5c0e988f20310642
XsideTh <akarawin45@gmail.com>
2023-09-26 15:00:54 +0700

    Implement model and some save

--- deepmycosis_application/lib/home_screen.dart
+++ deepmycosis_application/lib/home_screen.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,6 +16,47 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late CameraController controller;
+
+  late File _image;
+  List? _results;
+  bool imageSelect = false;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+
+  Future loadModel() async {
+    String res;
+    res = (await Tflite.loadModel(
+        model: "assets/model/dm_0.0001_16.tflite",
+        labels: "assets/model/labels.txt"))!;
+    print("Models loading status: $res");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Tflite.close();
+  }
+
+  Future imageClassification(File image) async {
+    final List? recognitions = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.1,
+      imageStd: 1,
+    );
+    print("Models evaluated : ${recognitions.toString()}");
+    setState(() {
+      _results = recognitions!;
+      _image = image;
+      imageSelect = true;
+      isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,6 +100,19 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  Future pickImage(ImageSource source) async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? pickedFile = await _picker.pickImage(
+      source: source,
+    );
+    setState(() {
+      isLoading = true;
+      imageSelect = false;
+    });
+    File image = File(pickedFile!.path);
+    imageClassification(image);
+  }
+
   Future<void> initialzationCamera() async {
     var cameras = await availableCameras();
     controller = CameraController(
@@ -64,18 +121,32 @@ class _HomeScreenState extends State<HomeScreen> {
     await controller.initialize();
   }
 
+  Future<String> getFilePath() async {
+    Directory appDocumentsDirectory =
+        await getApplicationDocumentsDirectory(); // 1
+    String appDocumentsPath = appDocumentsDirectory.path; // 2
+    String filePath = '$appDocumentsPath/sample.png'; // 3
+
+    return filePath;
+  }
+
+  void saveFile(var contents) async {
+    File file = File(await getFilePath()); // 1
+    file.writeAsBytes(contents); // 2
+  }
+
   onTakePicture() async {
-    await controller.takePicture().then((XFile xfile) {
+    await controller.takePicture().then((XFile xfile) async {
       if (mounted) {
         // ignore: unnecessary_null_comparison
         if (xfile != null) {
           var crop_image = new SizedBox(
             child: AspectRatio(
               aspectRatio: 487 / 300,
-              child: Container(height: 200,
+              child: Container(
+                height: 200,
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                    
                       fit: BoxFit.fitWidth,
                       // fitWidth ให้รูปเป็นแนวนอน
                       alignment: FractionalOffset.center,
@@ -84,10 +155,25 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           );
-          showDialog(
-              context: context,
-              builder: (context) =>
-                  AlertDialog(title: Text('test'), content: crop_image));
+          //saveFile(Image.file(File(xfile.path)).image);
+          await imageClassification(File(xfile.path));
+          var answer;
+          _results?.map((result) {
+            answer = Text(
+              "${result['label']}",
+            );
+          });
+          if (answer != null) {
+            showDialog(
+                context: context,
+                builder: (context) =>
+                    AlertDialog(title: Text('test'), content: Text(answer)));
+          } else {
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                    title: Text('test'), content: Text("Not Pythium")));
+          }
         }
       }
     });
