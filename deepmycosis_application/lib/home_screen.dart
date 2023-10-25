aca2d7f2e7890eb88c87f1b17a9a6305782a32a5
XsideTh <akarawin45@gmail.com>
2023-10-25 11:07:34 +0700

    crop_only_inside

--- deepmycosis_application/lib/home_screen.dart
+++ deepmycosis_application/lib/home_screen.dart
@@ -144,7 +144,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Future<String> _resizePhoto(String filePath) async {
     File croppedFile =
-        await FlutterNativeImage.cropImage(filePath, 170, 70, 265, 350);
+        await FlutterNativeImage.cropImage(filePath, 224, 154, 175, 175);
 
     return Future.value(croppedFile.path);
   }
