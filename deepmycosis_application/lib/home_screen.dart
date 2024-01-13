f257c25d907aafaed1897d8a3e3acb33b0734d07
XsideTh <144082841+XsideTh@users.noreply.github.com>
2024-01-13 12:55:48 +0700

    fix pick gallery

--- deepmycosis_application/lib/home_screen.dart
+++ deepmycosis_application/lib/home_screen.dart
@@ -33,46 +33,12 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    loadModel();
-  }
-
-  Future loadModel() async {
-    try {
-      //โหลด model โดยการใช้
-      classificationModel = await PytorchLite.loadClassificationModel(
-          //path model ความกว้าง ความสูง ของรูป
-          "assets/model/model.pt",
-          224,
-          224,
-          2,
-          labelPath: "assets/model/labels.txt"); //และ path label
-    } catch (e) {
-      if (e is PlatformException) {
-        print("only supported for android, Error is $e");
-      } else {
-        print("Error is $e");
-      }
-    }
   }
 
   @override
   void dispose() {
     super.dispose();
     //Tflite.close();
-  }
-
-  Future imageClassification(File image) async {
-    List<String> imagePrediction = await classificationModel
-        .getImagePrediction(await File(image.path).readAsBytes());
-    print("prediction is : ${imagePrediction[0]}");
-    print("with prob is : ${imagePrediction[1]}");
-    setState(() {
-      _results = imagePrediction[0];
-      _prob = double.parse(imagePrediction[1]);
-      _image = image;
-      imageSelect = true;
-      isLoading = false;
-    });
   }
 
   @override
@@ -106,8 +72,9 @@ class _HomeScreenState extends State<HomeScreen> {
       source: source,
     );
 
-    context.goNamed(modeling.routeName, params: {'image': pickedFile!.path});
-    dispose();
+    context.goNamed(modeling.routeName, queryParams: {
+      'image': pickedFile!.path,
+    });
     /*
     setState(() {
       isLoading = true;
@@ -118,32 +85,5 @@ class _HomeScreenState extends State<HomeScreen> {
     await imageClassification(image);
 
     resultShow(pickedFile!.path);*/
-  }
-
-  void resultShow(String image) {
-    if (_results != null) {
-      var prob = _prob;
-      var probResult;
-      if (prob! < 0.318) {
-        prob = 1 - prob;
-      }
-      if (prob! > 0.87) {
-        probResult = "There is High probability That this is " + _results!;
-      } else if (prob! > 0.5 && prob! < 0.87) {
-        probResult = "There is Medium probability That this is " + _results!;
-      } else {
-        probResult = "There is Low probability That this is " + _results!;
-      }
-
-      context.goNamed(ResultScreen.routeName, queryParams: {
-        'image': image,
-        'result': probResult,
-      });
-    } else {
-      showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-              title: Text('Result'), content: Text("error result is null")));
-    }
   }
 }
